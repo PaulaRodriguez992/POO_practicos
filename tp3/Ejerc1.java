d2b7e17b3428505f7e0ddfa28d519ec469b38408
paula.rodriguez992 <mariapaulaursula@gmail.com>
2025-04-25 10:16:20 -0300

    agregue 4 ejercicios

---
+++ tp3/Ejerc1.java
@@ -0,0 +1,30 @@
+/*Escriba un programa que lea tres números y si el primero es positivo calcule el producto de los otros dos,
+en otro caso calcule la suma.*/
+
+import java.util.Scanner;
+
+public class Ejerc1{
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in) ;
+		
+		System.out.println ("ingrese un numero");
+		int num1 = scanner.nextInt();
+		System.out.println ("ingrese un numero");
+		int num2 = scanner.nextInt();
+		System.out.println ("ingrese un numero");
+		int num3 = scanner.nextInt();
+		int result;
+		
+		if (num1 >= 0) {
+			result= num2 * num3 ;
+		}else {
+			result= num2 + num3;
+		}
+		System.out.println ("el resultado es:"+ result);
+		
+		scanner.close();
+			
+	}
+
+}
---
+++ tp3/Ejerc2.java
@@ -0,0 +1,27 @@
+/*Clasificador de Calificaciones:
+• Ingresa una calificación numérica (por ejemplo, de 0 a 10).
+Utiliza una serie de if-else if-else para clasificar la calificación en categorías como "Aprobado", "Reprobado",
+"Sobresaliente", etc. Define tus propios rangos.*/
+
+import java.util.Scanner;
+
+public class Ejerc2{
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in) ;
+       
+        System.out.println ("ingrese una calificacion del 1 al 10: ");
+        int calificacion = scanner.nextInt();
+            
+        if (calificacion >= 9) {
+            System.out.println ("Sobresaliente");
+        } else if (calificacion >= 7) {
+            System.out.println ("Aprobado");
+        } else if (calificacion == 6) {
+            System.out.println ("Regular");
+        } else {
+            System.out.println ("Reprobado");
+        }
+        scanner.close();
+    }
+}
